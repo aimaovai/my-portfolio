9dc0c4c692cebbf4c87d5810aec5c7ff72631a0f
Aima Ovai <ovaiaima@gmail.com>
2024-09-13 15:43:26 -0500

    fix apostrophe error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function LandingPage() {
   return (
     <div className="background">
      <div className="domain-welcome">
-          <text>Welcome to Aims's Domain</text>
+          <text>Welcome to Aims`&apos;`s Domain</text>
           <div>
             <button className="enter-button" onClick={() => router.push("/home")}>Click to enter</button>
           </div>
