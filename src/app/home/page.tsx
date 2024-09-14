f20144034f7ba289f7f01830ef3bb7ab0ee70d47
aimaovai <74068109+aimaovai@users.noreply.github.com>
2024-09-14 15:37:45 -0500

    Create home page (#5)

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -6,7 +6,10 @@ export default function Home() {
     return (
         <div className="background">
             <NavBar />
-            <div className="domain-welcome">
+            <div style={{ color: "white", fontSize: "xx-large", margin: "20% 10% 0 10%", left:0, zIndex: 1, position: "relative", top: "30%"
+            , textAlign: "center", height: "100vh",
+             }}>
+                Welcome! Page still in progress. Come back later.
             </div>
         </div>
     );
