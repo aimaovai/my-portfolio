2c9724906c4993c8c8ecdd6e8a1fb3141023d92f
Aima Ovai <ovaiaima@gmail.com>
2024-09-16 00:33:20 -0500

    Add place holders for home and projects pages

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
             <div style={{ color: "white", fontSize: "xx-large", margin: "20% 10% 0 10%", left:0, zIndex: 1, position: "relative", top: "30%"
             , textAlign: "center", height: "100vh",
              }}>
-                Welcome! Page still in progress. Come back later.
+                Welcome! Page still in progress. Check out other pages in the main time!
             </div>
         </div>
     );
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,11 @@ export default function Projects() {
     return (
         <div className="background">
             <NavBar />
-            Projects
+            <div style={{ color: "white", fontSize: "xx-large", margin: "20% 10% 0 10%", left:0, zIndex: 1, position: "relative", top: "30%"
+            , textAlign: "center", height: "100vh", wordWrap: "break-word",
+             }}>
+                Welcome! My projects page is still in progress. Come back later, and check out other pages in the mean time.
+            </div>
         </div>
     );
 }
