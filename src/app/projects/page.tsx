1f135cb6667104df84e606953c9d6b15093ed394
Aima Ovai <ovaiaima@gmail.com>
2024-09-16 16:34:13 -0500

    Add temp github link to projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "@/components/NavBar/NavBar";
+import Link from "next/link";
 
 export default function Projects() {    
     return (
@@ -8,8 +9,11 @@ export default function Projects() {
             <div style={{ color: "white", fontSize: "xx-large", margin: "20% 10% 0 10%", left:0, zIndex: 1, position: "relative", top: "30%"
             , textAlign: "center", height: "100vh", wordWrap: "break-word",
              }}>
+                <div style={{paddingBottom: "10px", textDecorationLine: "underline"}}>
+                <Link href={"https://github.com/aimaovai"}>My Github</Link>
+                </div>
                 Welcome! My projects page is still in progress. 
-                Come back later, and check out other pages in the mainn time.
+                Come back later, and check out other pages in the main time.
             </div>
         </div>
     );
