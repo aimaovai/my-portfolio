8b8d07e875b4845c8a5ee3098c4692e25a3345a8
aimaovai <74068109+aimaovai@users.noreply.github.com>
2024-09-27 04:49:50 -0500

    Update Projects page (#7)
    * Add pages and routes for projects
    
    * Style projects page and remove commented code from files as needed

--- src/components/MyResume/MyResume.js
+++ src/components/MyResume/MyResume.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Image from "next/image";
-import resumeImage from '/public/images/resume.jpg'
+import resumePg1 from '/public/images/resume_Page_1.jpg';
+import resumePg2 from '/public/images/resume_Page_2.jpg';
 
 export default function MyResume() {
     return (
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
             <Image
-                src={resumeImage}
-                alt="Resume"
+                src={resumePg1}
+                alt="Resume page 1"
+                height={900}
+                width={800} />
+            <Image
+                src={resumePg2}
+                alt="Resume page 2"
                 height={900}
                 width={800} />
         </div>);
