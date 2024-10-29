952fcb88088522e3edff9890fb19a180006a4252
Aima Ovai <ovaiaima@gmail.com>
2024-10-28 23:40:53 -0500

    Changes background to video background in app layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
-'use client'
-
+"use client";
 import { useRouter } from "next/navigation";
 import { Button } from "@nextui-org/button";
-
 export default function LandingPage() {
- 
-
-    const router = useRouter();
- 
+  const router = useRouter();
   return (
-    <div className="background2">
-     <div className="domain_welcome">
-          <text>Welcome to Aima&apos;s Domain</text>
-          <div className="enter_button">
-            <Button color="default" onClick={() => router.push("/home")} style={{ fontSize: "x-large", height: "50px"}}>Click to enter</Button>
-          </div>
-        </div>
+    <div className="landing-page">
+      {/* <div className="domain_welcome"> */}
+      <text>Welcome to Aima&apos;s Domain</text>
+      <div className="enter_button">
+        <Button
+          color="default"
+          onClick={() => router.push("/home")}
+          style={{ fontSize: "x-large", height: "50px" }}
+        >
+          Click to enter
+        </Button>
+      </div>
+      {/* </div> */}
     </div>
   );
 }
