1f949b2ee3b7ce2484db5311fb8a5174074ee0f3
Aima Ovai <ovaiaima@gmail.com>
2024-10-29 14:45:26 -0500

    Adds page for ui design projects

--- src/app/uiprototypes/page.tsx
+++ src/app/uiprototypes/page.tsx
@@ -1,21 +1,42 @@
 import React from "react";
 import NavBar from "@/components/NavBar/NavBar";
 import Link from "next/link";
+import Image from "next/image";
+import styles from "./uiprototypes.module.css";
 // import { useRouter } from "next/navigation";
 
 export default function UIPrototypes() {
-    // const router = useRouter();
-    return (
-        <div className="background2">
-            <NavBar />
-            <div style={{ color: "white", fontSize: "xx-large", margin: "20% 10% 0 10%", left:0, zIndex: 1, position: "relative", textAlign: "center", height: "50%", wordWrap: "break-word",
-             }}>
-                <div style={{paddingBottom: "10px", textDecorationLine: "underline"}}>
-                <Link href={"https://www.figma.com/@aimaovai"}>My Figma Profile</Link>
-                </div>
-                Welcome! My ui projects page is still in progress. 
-                Come back later, and check out other pages in the main time.
-            </div>
+  // const router = useRouter();
+  return (
+    <div className="content">
+      <NavBar />
+      <div className="content-body">
+        <div
+        className={styles.imageContainer}
+        >
+          <div>
+
+            <Image
+              src="/images/portfoliodesktop.png"
+              alt="logo"
+              width={300}
+              height={300}
+              style={{ border: "1px solid white" }}
+            />
+            <Link href="https://www.figma.com/design/E02J8NqmmWPSeLiYOrYqsb/Aima's-Website?node-id=0-1&t=uMy8L2ot21kJzU0I-1">My Portfolio</Link>
+          </div>
+          <div>
+            <Image
+              src="/images/triplez-desktop.png"
+              alt="logo"
+              width={300}
+              height={300}
+              style={{ border: "1px solid white" }}
+            />
+            <Link href="https://www.figma.com/design/97Ox4OAKMJcBcndeu6ZbS7/Triple-Z-Site?node-id=1-51&t=uMy8L2ot21kJzU0I-1">Triple Z Home Solutions</Link>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
