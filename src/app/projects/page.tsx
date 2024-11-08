87cd112ebc5ab0b5f458992f88d646e3779722e0
Aima Ovai <ovaiaima@gmail.com>
2024-11-08 15:39:40 -0600

    Fixes styling and page responsiveness for different screens

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,36 +1,47 @@
-'use client'
+"use client";
 
 import React from "react";
 import NavBar from "@/components/NavBar/NavBar";
 import ProjectCard from "@/components/ProjectCard/ProjectCard";
-import uiImage from '../../../public/images/ui.jpg';
-import githubOfcLogo from '../../../public/images/github-mark-white.png'
-import graphicDesginImage from '../../../public/images/graphic-design.jpg';
-import styles from './projects.module.css';
+import uiImage from "../../../public/images/ui.jpg";
+import githubOfcLogo from "../../../public/images/github-mark-white.png";
+import graphicDesginImage from "../../../public/images/graphic-design.jpg";
+import styles from "./projects.module.css";
+import { BiArrowBack } from "react-icons/bi";
+import { useRouter } from "next/navigation";
 
 export default function Projects() {
-    return (
-        <div className="content">
-            <NavBar />
-            <div className="content-body">
-                <div className={styles.container}>
-                    <ProjectCard
-                        image={uiImage}
-                        imageCaption={"UI projects"}
-                        link="/uiprototypes"
-                    />
-                    <ProjectCard
-                        image={githubOfcLogo}
-                        imageCaption={"GitHub projects"}
-                        link="https://github.com/aimaovai"
-                    />
-                    <ProjectCard
-                        image={graphicDesginImage}
-                        imageCaption={"Graphic design projects"}
-                        link="/graphicdesign"
-                    />
-                </div>
-            </div>
+    const router = useRouter();
+  return (
+    <div className="content">
+      <NavBar />
+      <div className={styles.container}>
+        <div className={styles["back-button"]}>
+          <BiArrowBack onClick={() => router.push("/home")} />
         </div>
-    );
+        <div className={styles["card-container"]}>
+          <ProjectCard
+            image={uiImage}
+            imageCaption={"UI projects"}
+            link="/uiprototypes"
+            cardDescription={
+              "Discover my collection of UI projects, each crafted with a focus on intuitive design, responsive layouts, and smooth interactions. Click through to see how I bring user experiences to life, combining functionality with style in every detail!"
+            }
+          />
+          <ProjectCard
+            image={githubOfcLogo}
+            imageCaption={"GitHub projects"}
+            link="https://github.com/aimaovai"
+            cardDescription={"Check out my GitHub projects—a mix of coding experiments, problem-solving solutions, and full-stack applications. Dive in to see my work in action, from backend logic to polished user interfaces!"}
+          />
+          <ProjectCard
+            image={graphicDesginImage}
+            imageCaption={"Graphic design projects"}
+            link="/graphicdesign"
+            cardDescription={"Explore my graphic design portfolio, featuring bold visuals, creative layouts, and a flair for detail. See how I bring ideas to life through impactful designs tailored for digital and print!"}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,22 +1,33 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import styles from './projectcard.module.css';
+import styles from "./projectcard.module.css";
 
-export default function ProjectCard({ image, imageCaption, link}) {
-    return (
-        <div className={styles.card}>
-            <Link href={link}>
-                <Image
-                    src={image}
-                    alt={imageCaption}
-                    height={200}
-                    // width="100%"
-                    maxWidth="100%"
-                    className={styles.image}
-                />
-                <div className="caption">{imageCaption}</div>
-            </Link>
+export default function ProjectCard({
+  image,
+  imageCaption,
+  link,
+  cardDescription,
+}) {
+  return (
+    <div className={styles["card-container"]}>
+    <Link href={link}>
+      <div className={styles.card}>
+        <div className={styles["image-container"]}>
+          <Image
+            src={image}
+            alt={imageCaption}
+            height={150}
+            // width="80%"
+            // maxWidth="80%"
+          />
         </div>
-    )
+        <div className={styles.caption}>{imageCaption}</div>
+        <div className={styles["card-description"]}>
+            <p>{cardDescription}</p>
+        </div>
+      </div>
+    </Link>
+ </div>
+  );
 }
